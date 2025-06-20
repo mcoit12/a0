ehlowr0ld/agent-zero

feat: memory consolidation

python/tools/knowledge_tool.py
@@ -1,4 +1,3 @@
-import os
 import asyncio
 from python.helpers import dotenv, memory, perplexity_search, duckduckgo_search
 from python.helpers.tool import Tool, Response
@@ -12,29 +11,31 @@ SEARCH_ENGINE_RESULTS = 10
 
 class Knowledge(Tool):
     async def execute(self, question="", **kwargs):
-        # Create tasks for all three search methods
+        if not question:
+            question = kwargs.get("query", "")
+            if not question:
+                return Response(message="No question provided", break_loop=False)
+
+        # Create tasks for all search methods
         tasks = [
             self.searxng_search(question),
             # self.perplexity_search(question),
             # self.duckduckgo_search(question),
-            self.mem_search(question),
+            self.mem_search_enhanced(question),
         ]
 
         # Run all tasks concurrently
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
-        # perplexity_result, duckduckgo_result, memory_result = results
+        # searxng_result, memory_result = results
         searxng_result, memory_result = results
 
         # Handle exceptions and format results
-        # perplexity_result = self.format_result(perplexity_result, "Perplexity")
-        # duckduckgo_result = self.format_result(duckduckgo_result, "DuckDuckGo")
         searxng_result = self.format_result_searxng(searxng_result, "Search Engine")
         memory_result = self.format_result(memory_result, "Memory")
 
         msg = self.agent.read_prompt(
             "tool.knowledge.response.md",
-            #   online_sources = ((perplexity_result + "\n\n") if perplexity_result else "") + str(duckduckgo_result),
             online_sources=((searxng_result + "\n\n") if searxng_result else ""),
             memory=memory_result,
         )
@@ -45,10 +46,10 @@ class Knowledge(Tool):
 
         return Response(message=msg, break_loop=False)
 
-    async def perplexity_search(self, question):
+    async def perplexity_search(self, query):
         if dotenv.get_dotenv_value("API_KEY_PERPLEXITY"):
             return await asyncio.to_thread(
-                perplexity_search.perplexity_search, question
+                perplexity_search.perplexity_search, query
             )
         else:
             PrintStyle.hint(
@@ -60,19 +61,148 @@ class Knowledge(Tool):
             )
             return None
 
-    async def duckduckgo_search(self, question):
-        return await asyncio.to_thread(duckduckgo_search.search, question)
+    async def duckduckgo_search(self, query):
+        return await asyncio.to_thread(duckduckgo_search.search, query)
 
-    async def searxng_search(self, question):
-        return await searxng(question)
+    async def searxng_search(self, query):
+        return await searxng(query)
 
-    async def mem_search(self, question: str):
+    async def mem_search(self, query: str):
+        """Legacy memory search method for backward compatibility."""
         db = await memory.Memory.get(self.agent)
         docs = await db.search_similarity_threshold(
-            query=question, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD
+            query=query, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD
         )
         text = memory.Memory.format_docs_plain(docs)
         return "\n\n".join(text)
+
+    async def mem_search_enhanced(self, query: str):
+        """
+        Enhanced memory search with knowledge source awareness.
+        Separates and prioritizes knowledge sources vs conversation memories.
+        """
+        try:
+            db = await memory.Memory.get(self.agent)
+
+            # Search for knowledge sources (knowledge_source=True)
+            knowledge_docs = await db.search_similarity_threshold(
+                query=query, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD,
+                filter="knowledge_source == True"
+            )
+
+            # Search for conversation memories (field doesn't exist or is not True)
+            conversation_docs = await db.search_similarity_threshold(
+                query=query, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD,
+                filter="not knowledge_source if 'knowledge_source' in locals() else True"
+            )
+
+            # Combine and fallback to lower threshold if needed
+            all_docs = knowledge_docs + conversation_docs
+            threshold_note = ""
+
+            # If no results with default threshold, try with lower threshold
+            if not all_docs:
+                lower_threshold = DEFAULT_MEMORY_THRESHOLD * 0.8
+                knowledge_docs = await db.search_similarity_threshold(
+                    query=query, limit=5, threshold=lower_threshold,
+                    filter="knowledge_source == True"
+                )
+                conversation_docs = await db.search_similarity_threshold(
+                    query=query, limit=5, threshold=lower_threshold,
+                    filter="not knowledge_source if 'knowledge_source' in locals() else True"
+                )
+                all_docs = knowledge_docs + conversation_docs
+                if all_docs:
+                    threshold_note = f" (threshold: {lower_threshold})"
+
+            if not all_docs:
+                return await self._get_memory_diagnostics(db, query)
+
+            # Separate knowledge sources from conversation memories
+            knowledge_sources = knowledge_docs
+            conversation_memories = conversation_docs
+            result_parts = []
+
+            # Add search summary
+            result_parts.append(f"## 🔍 Search Results for: '{query}'")
+            result_parts.append(f"**Found:** {len(knowledge_sources)} knowledge sources, {len(conversation_memories)} conversation memories{threshold_note}")
+
+            # Show knowledge sources
+            if knowledge_sources:
+                result_parts.append("")
+                result_parts.append("## 📚 Knowledge Sources:")
+                for index, doc in enumerate(knowledge_sources):
+                    source_file = doc.metadata.get('source_file', 'Unknown source')
+                    file_type = doc.metadata.get('file_type', '').upper()
+                    area = doc.metadata.get('area', 'main').upper()
+
+                    result_parts.append(f"**Source:** {source_file} ({file_type}) [{area}]")
+                    result_parts.append(f"**Content:** {doc.page_content}")
+                    if index < len(knowledge_sources) - 1:
+                        result_parts.append("-" * 80)
+
+            # Show conversation memories
+            if conversation_memories:
+                if knowledge_sources:
+                    result_parts.append("")
+                result_parts.append("## 💭 Related Experience:")
+                for index, doc in enumerate(conversation_memories):
+                    timestamp = doc.metadata.get('timestamp', 'Unknown time')
+                    area = doc.metadata.get('area', 'main').upper()
+                    consolidation_action = doc.metadata.get('consolidation_action', '')
+
+                    metadata_info = f"{timestamp} [{area}]"
+                    if consolidation_action:
+                        metadata_info += f" (consolidated: {consolidation_action})"
+
+                    result_parts.append(f"**Experience:** {metadata_info}")
+                    result_parts.append(f"**Content:** {doc.page_content}")
+                    if index < len(conversation_memories) - 1:
+                        result_parts.append("-" * 80)
+
+            return "\n".join(result_parts)
+
+        except Exception as e:
+            handle_error(e)
+            return f"Memory search failed: {str(e)}"
+
+    async def _get_memory_diagnostics(self, db, query: str):
+        """Provide memory diagnostics when no search results are found."""
+        try:
+            # Get sample of all documents to see what's in memory
+            sample_docs = await db.search_similarity_threshold(
+                query="test", limit=20, threshold=0.0
+            )
+
+            if not sample_docs:
+                return f"## 🔍 No Results for: '{query}'\n**Memory database appears to be empty.**"
+
+            # Analyze what's in memory
+            area_counts: dict[str, int] = {}
+            knowledge_count = 0
+
+            for doc in sample_docs:
+                area = doc.metadata.get('area', 'unknown')
+                area_counts[area] = area_counts.get(area, 0) + 1
+                if doc.metadata.get('knowledge_source', False):
+                    knowledge_count += 1
+
+            result_parts = [
+                f"## 🔍 No Results for: '{query}'",
+                f"**Database contains:** {len(sample_docs)} total documents",
+                f"**Areas:** {', '.join([f'{area.upper()}: {count}' for area, count in area_counts.items()])}",
+                f"**Knowledge sources:** {knowledge_count} documents",
+                "",
+                "**Suggestions:**",
+                "- Try different or more general search terms",
+                "- Check if the information was recently memorized",
+                f"- Current search threshold: {DEFAULT_MEMORY_THRESHOLD}"
+            ]
+
+            return "\n".join(result_parts)
+
+        except Exception as e:
+            return f"Memory diagnostics failed: {str(e)}"
 
     def format_result(self, result, source):
         if isinstance(result, Exception):
@@ -84,6 +214,9 @@ class Knowledge(Tool):
         if isinstance(result, Exception):
             handle_error(result)
             return f"{source} search failed: {str(result)}"
+
+        if not result or "results" not in result:
+            return ""
 
         outputs = []
         for item in result["results"]:

mcoit12/a0

python/tools/knowledge_tool.py
@@ -1,4 +1,3 @@
-import os
 import asyncio
 from python.helpers import dotenv, memory, perplexity_search, duckduckgo_search
 from python.helpers.tool import Tool, Response
@@ -13,12 +12,17 @@ SEARCH_ENGINE_RESULTS = 10
 
 class Knowledge(Tool):
     async def execute(self, question="", **kwargs):
-        # Create tasks for all three search methods
+        if not question:
+            question = kwargs.get("query", "")
+            if not question:
+                return Response(message="No question provided", break_loop=False)
+
+        # Create tasks for all search methods
         tasks = [
             self.searxng_search(question),
             # self.perplexity_search(question),
             # self.duckduckgo_search(question),
-            self.mem_search(question),
+            self.mem_search_enhanced(question),
         ]
 
         # Run all tasks concurrently
@@ -31,8 +35,6 @@ class Knowledge(Tool):
         searxng_result = await self.searxng_document_qa(searxng_result, question)
 
         # Handle exceptions and format results
-        # perplexity_result = self.format_result(perplexity_result, "Perplexity")
-        # duckduckgo_result = self.format_result(duckduckgo_result, "DuckDuckGo")
         searxng_result = self.format_result_searxng(searxng_result, "Search Engine")
         memory_result = self.format_result(memory_result, "Memory")
 
@@ -102,6 +104,134 @@ class Knowledge(Tool):
         text = memory.Memory.format_docs_plain(docs)
         return "\n\n".join(text)
 
+    async def mem_search_enhanced(self, question: str):
+        """
+        Enhanced memory search with knowledge source awareness.
+        Separates and prioritizes knowledge sources vs conversation memories.
+        """
+        try:
+            db = await memory.Memory.get(self.agent)
+
+            # Search for knowledge sources (knowledge_source=True)
+            knowledge_docs = await db.search_similarity_threshold(
+                query=question, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD,
+                filter="knowledge_source == True"
+            )
+
+            # Search for conversation memories (field doesn't exist or is not True)
+            conversation_docs = await db.search_similarity_threshold(
+                query=question, limit=5, threshold=DEFAULT_MEMORY_THRESHOLD,
+                filter="not knowledge_source if 'knowledge_source' in locals() else True"
+            )
+
+            # Combine and fallback to lower threshold if needed
+            all_docs = knowledge_docs + conversation_docs
+            threshold_note = ""
+
+            # If no results with default threshold, try with lower threshold
+            if not all_docs:
+                lower_threshold = DEFAULT_MEMORY_THRESHOLD * 0.8
+                knowledge_docs = await db.search_similarity_threshold(
+                    query=question, limit=5, threshold=lower_threshold,
+                    filter="knowledge_source == True"
+                )
+                conversation_docs = await db.search_similarity_threshold(
+                    query=question, limit=5, threshold=lower_threshold,
+                    filter="not knowledge_source if 'knowledge_source' in locals() else True"
+                )
+                all_docs = knowledge_docs + conversation_docs
+                if all_docs:
+                    threshold_note = f" (threshold: {lower_threshold})"
+
+            if not all_docs:
+                return await self._get_memory_diagnostics(db, question)
+
+            # Separate knowledge sources from conversation memories
+            knowledge_sources = knowledge_docs
+            conversation_memories = conversation_docs
+            result_parts = []
+
+            # Add search summary
+            result_parts.append(f"## 🔍 Search Results for: '{question}'")
+            result_parts.append(f"**Found:** {len(knowledge_sources)} knowledge sources, {len(conversation_memories)} conversation memories{threshold_note}")
+
+            # Show knowledge sources
+            if knowledge_sources:
+                result_parts.append("")
+                result_parts.append("## 📚 Knowledge Sources:")
+                for index, doc in enumerate(knowledge_sources):
+                    source_file = doc.metadata.get('source_file', 'Unknown source')
+                    file_type = doc.metadata.get('file_type', '').upper()
+                    area = doc.metadata.get('area', 'main').upper()
+
+                    result_parts.append(f"**Source:** {source_file} ({file_type}) [{area}]")
+                    result_parts.append(f"**Content:** {doc.page_content}")
+                    if index < len(knowledge_sources) - 1:
+                        result_parts.append("-" * 80)
+
+            # Show conversation memories
+            if conversation_memories:
+                if knowledge_sources:
+                    result_parts.append("")
+                result_parts.append("## 💭 Related Experience:")
+                for index, doc in enumerate(conversation_memories):
+                    timestamp = doc.metadata.get('timestamp', 'Unknown time')
+                    area = doc.metadata.get('area', 'main').upper()
+                    consolidation_action = doc.metadata.get('consolidation_action', '')
+
+                    metadata_info = f"{timestamp} [{area}]"
+                    if consolidation_action:
+                        metadata_info += f" (consolidated: {consolidation_action})"
+
+                    result_parts.append(f"**Experience:** {metadata_info}")
+                    result_parts.append(f"**Content:** {doc.page_content}")
+                    if index < len(conversation_memories) - 1:
+                        result_parts.append("-" * 80)
+
+            return "\n".join(result_parts)
+
+        except Exception as e:
+            handle_error(e)
+            return f"Memory search failed: {str(e)}"
+
+    async def _get_memory_diagnostics(self, db, query: str):
+        """Provide memory diagnostics when no search results are found."""
+        try:
+            # Get sample of all documents to see what's in memory
+            sample_docs = await db.search_similarity_threshold(
+                query="test", limit=20, threshold=0.0
+            )
+
+            if not sample_docs:
+                return f"## 🔍 No Results for: '{query}'\n**Memory database appears to be empty.**"
+
+            # Analyze what's in memory
+            area_counts: dict[str, int] = {}
+            knowledge_count = 0
+
+            for doc in sample_docs:
+                area = doc.metadata.get('area', 'unknown')
+                area_counts[area] = area_counts.get(area, 0) + 1
+                if doc.metadata.get('knowledge_source', False):
+                    knowledge_count += 1
+
+            result_parts = [
+                f"## 🔍 No Results for: '{query}'",
+                f"**Database contains:** {len(sample_docs)} total documents",
+                f"**Areas:** {', '.join([f'{area.upper()}: {count}' for area, count in area_counts.items()])}",
+                f"**Knowledge sources:** {knowledge_count} documents",
+                "",
+                "**Suggestions:**",
+                "- Try different or more general search terms",
+                "- Check if the information was recently memorized",
+                f"- Current search threshold: {DEFAULT_MEMORY_THRESHOLD}"
+            ]
+
+            return "\n".join(result_parts)
+
+        except Exception as e:
+            return f"Memory diagnostics failed: {str(e)}"
+
     def format_result(self, result, source):
         if isinstance(result, Exception):
             handle_error(result)
@@ -112,6 +242,9 @@ class Knowledge(Tool):
         if isinstance(result, Exception):
             handle_error(result)
             return f"{source} search failed: {str(result)}"
+
+        if not result or "results" not in result:
+            return ""
 
         outputs = []
         for item in result["results"]: